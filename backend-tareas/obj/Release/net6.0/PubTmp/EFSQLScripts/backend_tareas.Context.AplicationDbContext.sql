4c498717a0afe21d60a4c4fe46921faf406407fc
lucionigro <lucionigrot@gmail.com>
2023-01-30 15:20:25 -0300

    deploy del backend

---
+++ backend-tareas/obj/Release/net6.0/PubTmp/EFSQLScripts/backend_tareas.Context.AplicationDbContext.sql
@@ -0,0 +1,34 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230130125748_v1.0')
+BEGIN
+    CREATE TABLE [Tareas] (
+        [Id] int NOT NULL IDENTITY,
+        [Nombre] varchar(100) NOT NULL,
+        [Estado] bit NOT NULL,
+        CONSTRAINT [PK_Tareas] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230130125748_v1.0')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230130125748_v1.0', N'6.0.12');
+END;
+GO
+
+COMMIT;
+GO
+
